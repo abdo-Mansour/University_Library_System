9f45442d20d4fc8a9d39d0aab797ad3f247e9e5a
Dark <mohamed2003elashmawy@gmail.com>
2023-05-21 15:04:06 +0300

    implemented and edited some functions

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -3,18 +3,20 @@
 
 from Model.Person import Person
 from Model.Authenticator import Authenticator 
+from Model.Library import Library
 
 
 class Controller:
     def __init__(self):
         self.Person = None
         self.loggedIn = False
-        self.library = None
+        self.Library = None
         self.auth = Authenticator()
         pass
 
     # Functions related to normal user query
-    def logIn(self, email, password):
+    def logIn(self, email, password):       # DONE
+
         # Returns Exceptions if something wrong happened or true if success
         if self.auth.isAuth(email, password):
             self.Person = self.auth.returnPersonData(email, password)
@@ -27,7 +29,7 @@ class Controller:
             # View should take false and display it as error
             return False
 
-    def getBooksBy(self, query, value):
+    def getBooksBy(self, query, value):     # DONE
 
         if self.loggedIn == True:
             queries = ["bookGenre", "author", "authorsOfBook", "location"]
@@ -35,7 +37,7 @@ class Controller:
             if query in queries:
 
                 # this function should return a list of lists
-                data = checkDatabase(query, value)
+                data = Library.getBooksBy(query, value)
 
                 # View should display this returned data, also the data should be returned as a list
                 return data
@@ -44,44 +46,32 @@ class Controller:
                 # This should be displayed to the user as no books found
                 return False
 
-        """
-        Returns list of lists under given query and value
+    def getNBooks(self, book):              # DONE
 
-        Parameters:
-        self : self
-        query (string): the attribute you want to search about, for example "author"
-        value (string): the value of the attribute you want to search about, for example "El-Messery"
-
-        Returns:
-        int: list of lists where every sub-list has all the data of the book under given category
-        """
-        pass
-
-    def getNBooks(self, book):
         # returns dictionary where every sub-list has all the data of the book
         if self.loggedIn == True:
-            data = getDetails(book.id)
+            data = Library.getNBooks(book.id)
             if data:
                 return data
             else:
                 return False
 
-    def getLocation(self, bookID):
+    def getLocation(self, bookID):          # NOT DONE
+
         # returns location of the book
         if self.loggedIn == True:
             # I dont know how to fully implement this yet rn cause there are no functions in the database for this
             pass
         pass
 
-    def getUserDetails(self):
+    def getUserDetails(self):               # DONE
         if self.loggedIn == True:
-
             # Returns all data of user as a dictionary for view to use
             return self.Person.__dict__
 
-    def addStudent(self, studentInfo):
-        # Functions related to Admin Only Use
-        # studentInfo should be a list where data is [id, firstName, lastName, number, dob, sex, isAdmin, email, password]
+    # Functions related to Admin Only Use
+    def addStudent(self, studentInfo):      # DONE
+
         if self.loggedIn and self.isAdmin:
             newUser = Person(
                 id=studentInfo[0],
@@ -95,17 +85,18 @@ class Controller:
                 password=studentInfo[8]
             )
 
+            # returns a list where data is [id, firstName, lastName, number, dob, sex, isAdmin, email, password]
             self.auth.addStudent(newUser)
 
-    def addBook(self, listOfBookDetails):
-        # like the login, throws exception if the data entered is not valid
-        pass
+    def addBook(self, listOfBookDetails):   # DONE
+        Library.addBook(listOfBookDetails)
+        return True
 
-    def updateBookDetails(self, bookInfo):
+    def updateBookDetails(self, bookInfo):  # NOT DONE
         # updates the book details
         pass
 
-    def updateUserDetails(self, userInfo):
+    def updateUserDetails(self, userInfo):  # NOT DONE
         # like the login, throws exception if the data entered is not valid
         pass
 
