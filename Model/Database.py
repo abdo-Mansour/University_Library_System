b5d469160b4664f0655580a7fb6f87a2136506ad
SuperNova74-K <kingkhaled.w@gmail.com>
2023-05-21 14:10:39 +0300

    Online Database V 1.0

--- Model/Database.py
+++ Model/Database.py
@@ -9,44 +9,50 @@ use this query to find out the server name:
 
 """
 
-# database object
 import pyodbc
 import sys
 
+
+# database object
 class Database:
     def __init__(self):
-        self._db = None
-        self.serverName = None
+        self.__serverName = None
         self.databaseName = None
         self.cursor = None
-
+        self.__username = None
+        self.__password = None
+        self.__driver = None
 
     def connectToDataBase(self):
-        #used to connect to the database
-        #this function will ask the user for the server name and the database name
-        
-        self.serverName = input("Enter the server name: ")
-        # self.serverName = "DESKTOP-VHJ2UQ7\SQLEXPRESS"
+        # used to connect to the database
+        # this function will ask the user for the server name and the database name
 
-        self.databaseName = input("Enter the database name: ")
-        # self.databaseName = "library"
-        
+        self.__serverName = 'SQL8005.site4now.net'
+
+        self.databaseName = 'db_a99989_librarysystem'
+
+        self.__username = 'db_a99989_librarysystem_admin'
+
+        self.__password = '$SzbsB6C1lte'
+
+        self.__driver = '{ODBC Driver 17 for SQL Server}'
+
         connection_string = f"""
-        DRIVER={"SQL SERVER"};
-        SERVER={self.serverName};
+        DRIVER={self.__driver};
+        SERVER={self.__serverName};
         DATABASE={self.databaseName};
+        UID={self.__username};
+        PWD={self.__password};
         Trust_Connection=yes;
         """
         try:
             cnxn = pyodbc.connect(connection_string)
             self.cursor = cnxn.cursor()
-            # confirmation message 
+            # confirmation message
         except Exception as e:
             print(e)
-            print("Task is Terminated!")
+            print("Connection To Data Base failed, make sure you have internet connection.")
             sys.exit(0)
-
-
 
     def closeConnection(self):
         self.cursor.close()
@@ -57,6 +63,3 @@ class Database:
 
     def getCursor(self):
         return self.cursor
-    
-    
-    
