f4b71cb3fd9a92051ac261e5fd3dcd05486914b6
Error message: AMFAM <abdulrahmanmfam2003@gmail.com>
2023-05-22 18:26:29 +0300

    now i know how to switch between frames nothing is going to stop us boys

--- View/App.py
+++ View/App.py
@@ -1,15 +1,105 @@
-from customtkinter import CTk, CTkButton
+ 
+LARGEFONT =("Verdana", 35)
+  
+import tkinter as tk
+from tkinter import ttk
+from StartPage import StartPage 
+class App(tk.Tk):
+    
+    # __init__ function for class tkinterApp
+    def __init__(self, *args, **kwargs):
+        print("I AM RUNNING")     
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
+         
+        # creating a container
+        container = ttk.Frame(self)
+        #set width and height
+        container.config(width=1000,height=580)
+        container.pack(side = "top", fill = "both", expand = True)
+  
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+  
+        self.frames = {}
+        for F in (StartPage, Page1, Page2):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-class App(CTk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("400x150")
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
 
-        self.button = CTkButton(self, text="my button", command=self.button_callbck)
-        self.button.pack(padx=20, pady=20)
-
-    def button_callbck(self):
-        print("button clicked")
-
+        self.show_frame("Page1")
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+  
+# first window frame startpage
+ 
+  
+  
+# second window frame page1
+class Page1(ttk.Frame):
+     
+    def __init__(self, parent, controller):
+         
+        ttk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text ="Page 1", font = LARGEFONT)
+        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+  
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text ="StartPage",
+                            command = lambda : controller.show_frame("StartPage"))
+     
+        # putting the button in its place
+        # by using grid
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+  
+        # button to show frame 2 with text
+        # layout2
+        button2 = ttk.Button(self, text ="Page 2",
+                            command = lambda : controller.show_frame("Page2"))
+     
+        # putting the button in its place by
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+  
+  
+  
+  
+# third window frame page2
+class Page2(ttk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text ="Page 2", font = LARGEFONT)
+        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+  
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text ="Page 1",
+                            command = lambda : controller.show_frame("Page1"))
+     
+        # putting the button in its place by
+        # using grid
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+  
+        # button to show frame 3 with text
+        # layout3
+        button2 = ttk.Button(self, text ="Startpage",
+                            command = lambda : controller.show_frame("StartPage"))
+     
+        # putting the button in its place by
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+  
+  
+# Driver Code
 app = App()
+app.geometry("1000x580")
 app.mainloop()
--- View/Login.py
+++ View/Login.py
@@ -1,16 +1,37 @@
 from customtkinter import CTkFrame,  CTkButton, CTkLabel
-
+import customtkinter as ctk
+import tkinter.messagebox as tkmb
 class Login(CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
         self.pack()
 
-        self.label = CTkLabel(self, text="Login", font=controller.title_font)
-        self.label.pack(side="top", fill="x", pady=10)
 
-        self.button = CTkButton(self, text="Back to Home", command=self.button_callbck)
-        self.button.pack(padx=20, pady=20)
+        self.label = ctk.CTkLabel(parent,text="Welcome To FCAI Library System",font=("Arial",30,"bold"))
+  
+        self.label.pack(pady=0)
+        
+        
+        self.frame = ctk.CTkFrame(master=parent)
+        self.frame.pack(pady=20,padx=40,fill='both')
+        
+        self.label = ctk.CTkLabel(master=self.frame,text='Please Enter Your Email and Password',font=("Arial",20,'bold'))
+        self.label.pack(pady=40,padx=10)
+        
+        
+        user_entry= ctk.CTkEntry(master=self.frame,width=300 , height= 40 ,placeholder_text="Email",font=("Arial",20))
+        user_entry.pack(pady=12,padx=10)
+        
+        user_pass= ctk.CTkEntry(master=self.frame,width=300 , height= 40 ,placeholder_text="Password",font=("Arial",20),show="*")
+        user_pass.pack(pady=12,padx=10)
+        
+        
+        button = ctk.CTkButton(master=self.frame,text='Login',font=("Arial",20,"bold"),command=login)
+        button.pack(pady=22,padx=10)
+        
+        checkbox = ctk.CTkCheckBox(master=self.frame,text='Login as Admin',font=("Arial",20))
+        checkbox.pack(pady=22,padx=10)
 
     def button_callbck(self):
         self.controller.show_frame("Home")
@@ -18,4 +39,15 @@ class Login(CTkFrame):
     def on_show(self):
         print("Login frame is shown")
         
+# # Selecting GUI theme - dark, light , system (for system default)
+# ctk.set_appearance_mode("dark")
+  
+# # Selecting color theme - blue, green, dark-blue
+# ctk.set_default_color_theme("dark-blue")
+  
+# app = ctk.CTk()
+# app.geometry("1000x580")
+# app.title("Library System")
+  
+  
 
